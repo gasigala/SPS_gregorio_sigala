0301ba71c7988300cc52964b391576051146ce74
Gregorio Sigala <omenrobot12@gmail.com>
2021-03-04 21:37:49 +0000

    fix jetty 9, make servelet in portfolio

---
+++ portfolio/src/main/java/com/google/sps/servlets/week2Servelet.java
@@ -0,0 +1,21 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that returns HTML that contains the page view count. */
+@WebServlet("/week2")
+public class week2Servelet extends HttpServlet {
+
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    response.setContentType("text/html;");
+    response.getWriter().println("<h1>This is a servelet example:</h1>");
+    response.getWriter().println("<p>Hello world!</p>");
+  }
+}
