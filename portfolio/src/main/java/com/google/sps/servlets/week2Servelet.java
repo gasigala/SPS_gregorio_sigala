de3e0205d61b38a5b0fa62ba587c751fddf7d63e
Gregorio Sigala <omenrobot12@gmail.com>
2021-03-06 00:23:27 +0000

    add array list, modify javascript

--- portfolio/src/main/java/com/google/sps/servlets/week2Servelet.java
+++ portfolio/src/main/java/com/google/sps/servlets/week2Servelet.java
@@ -1,10 +1,14 @@
 package com.google.sps.servlets;
 
+
 import java.io.IOException;
+import java.util.ArrayList;
+import com.google.gson.Gson;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /** Servlet that returns HTML that contains the page view count. */
 @WebServlet("/week2")
@@ -13,9 +17,19 @@ public class week2Servelet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    ArrayList<String> randomMessage= new ArrayList<String>();
+    randomMessage.add("Hello there!");
+    randomMessage.add("Hope you enjoy your stay.");
+    randomMessage.add("Stay tuned for what to come");
 
-    response.setContentType("text/html;");
-    response.getWriter().println("This is a servelet example:");
-    response.getWriter().println("Hello world!");
+    String json = convertToJsonUsingGson(randomMessage);
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+    
+  }
+    private String convertToJsonUsingGson(ArrayList<String> serverStats) {
+    Gson gson = new Gson();
+    String json = gson.toJson(serverStats);
+    return json;
   }
 }
